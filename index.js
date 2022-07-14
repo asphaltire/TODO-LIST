07d5ffcc327963f7d6f0fc64070ce804b78865d9
asphaltire <suvamsingh99@gmail.com>
2022-07-14 14:20:58 +0530

    rewrote clean javascript

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const output = document.querySelector("output");
-document.getElementById("list").onclick = function () {
-    var element = document.getElementById("input").value;
-    const taskline = document.createElement("li");
-    const del = document.createElement("button");
-   
-    const newcontent = document.createTextNode(element);
-    document.getElementById("input").value = "";
-    taskline.appendChild(newcontent);
-    document.body.insertBefore(taskline, output);
-    const text = document.createTextNode("delete");
-    del.appendChild(text);
-    document.body.insertBefore(del,output);
-    del.setAttribute('class','delete');
-    del.setAttribute('id','del');
-    taskline.setAttribute('class', 'taskline');
-    console.log(element);
-    input.focus();
+const addTask = document.getElementById("list");
+const task = document.getElementById("input");
+const list = document.getElementById("output");
+
+addTask.onclick = function (){
+        
+        const child = document.createElement('li');
+        child.textContent = task.value;
+        list.appendChild(child);
+        const del = document.createElement("button");
+        del.textContent = "delete";
+        list.appendChild(del);
+        del.setAttribute("class","delete");
+        task.value="";
+
+        del.onclick = function()
+        {
+            list.removeChild(child);
+            list.removeChild(del);
+        }
+        
+        task.focus();
+
 }
-
-
-// document.getElementById("del").onclick = function () {
-//     taskline.remove();
-// }
